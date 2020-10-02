d22cfb34406f4116ab64d276a3b824393ed48b1a
Oscar Polanco <opolanco@intellisys.com.do>
2020-10-02 01:00:04 -0400

    Create a layout component to encapsulate some of the elements that will be present in more than one page

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Nav from '../components/Nav';
+import Layout from '../components/Layout';
 
 export default function HomePage() {
   return (
-    <div>
-      <Nav />
+    <Layout>
       <p>Hey! I'm home page</p>
-    </div>
+    </Layout>
   );
 }
--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 export default function PizzasPage() {
   return (
-    <div>
+    <Layout>
       <p>Hey! I'm pizzas page page</p>
-    </div>
+    </Layout>
   );
 }
