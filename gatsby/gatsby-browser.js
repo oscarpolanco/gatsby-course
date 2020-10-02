72304cf7cd379523223b7c88e8e4e2b6b04c9e30
Oscar Polanco <opolanco@intellisys.com.do>
2020-10-02 19:50:24 -0400

    Import style to the gatsby-browser file to make  it globally avilable and some clean up

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from './src/components/Layout';
 
+import './src/styles/red.css';
+
 export function wrapPageElement({ element, props }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Layout {...props}>{element}</Layout>;
