496ba5db7fc4c824119f6675b01be04239bbf15f
Oscar Polanco <opolanco@intellisys.com.do>
2020-10-16 23:58:14 -0400

    Do a page query of all pizzas on the pizza page

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
   return (
     <>
-      <p>Hey! I'm pizzas page page</p>
+      <p>Hey! There are {data.pizzas.nodes.length} Pizzas!!</p>
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
